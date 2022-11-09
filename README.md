a2b3d6a81fc010ccda6e3752f85a8710564dab26
João Victor <jvmarcolino@outlook.com>
2022-11-08 21:32:55 -0300

    Adicionado temas com JS e ajustes css

---
+++ README.md
@@ -0,0 +1,21 @@
+## NLW eSports 
+
+![preview](./.github/preview.png)
+
+>Trilha Explorer
+
+Projeto construido no evento next level week da Pocketseat
+
+🔗[Clique aqui para acessar](https://jvictor08.github.io/nlw-copa-explorer/)
+
+## 🛠 Tecnologias 
+
+* HTML
+
+* CSS 
+
+* GIT E Github
+
+## ✉ Contato 
+
+jvmarcolino@outlook.com
--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function toYellow(){
+    let cleanclass = document.getElementById('corpo')
+    let classe = cleanclass.classList.value
+       if(cleanclass.classList != ''){
+        cleanclass.classList.remove(`${classe}`)
+    }
+} 
+    
+function toBlue(){
+    document.getElementById('corpo').classList.add("blue");
+}
+
+function toGreen(){
+    document.getElementById('corpo').classList.add("green");
+}
+
 function createGame (player1, hour, player2) {
     return `
     <li>
@@ -22,7 +38,13 @@ function createCard(date, day, games) {
 }
    
 document.querySelector(".cards").innerHTML=
-    createCard("21/11", "quinta", createGame('brazil','13:00','serbia') + createGame('japan','16:00','colombia') + createGame('switzerland','18:00','cameroon')) +
-    createCard("25/11","quinta",createGame('portugal','13:00','argentina') + createGame('brazil','16:00','southkorea') + createGame('serbia','18:00','japan')) +
-    createCard("25/11","quinta",createGame('portugal','13:00','argentina') + createGame('brazil','16:00','southkorea') + createGame('serbia','18:00','japan'))
+    createCard("21/11", "segunda", createGame('england','10:00','iran') + createGame('senegal','13:00','netherlands')) +
+    createCard("22/11","terça",createGame('argentina','7:00','arab') + createGame('france','16:00','australia')) +
+    createCard("23/11","quarta",createGame('germany','10:00','japan') + createGame('span','13:00','costarica') + createGame('belgium','16:00','canada'))+
+    createCard("24/11","quinta",createGame('uruguay','10:00','southkorea') + createGame('portugal','13:00','ghana') + createGame('brazil','16:00','serbia'))+
+    createCard("26/11","sábado",createGame('argentina','16:00','mexico')) +
+    createCard("27/11","domingo",createGame('span','16:00','germany')) +
+    createCard("28/11","segunda",createGame('brazil','13:00','switzerland') + createGame('portugal','16:00','uruguay'))+
+    createCard("01/12","quinta",createGame('croatia','12:00','belgium') + createGame('japan','16:00','span') + createGame('costarica','16:00','germany'))+
+    createCard("02/11","sexta",createGame('southkorea','12:00','portugal') + createGame('cameroon','16:00','brazil'))
 
